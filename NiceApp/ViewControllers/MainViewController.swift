be8d2fe869467d7428c401b47d13af3b7956ce04
hwiwls <hwiollfjh@gmail.com>
2023-06-26 23:14:37 +0900

    splash commit

--- NiceApp/ViewControllers/MainViewController.swift
+++ NiceApp/ViewControllers/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Lottie
 
 class MainViewController: UIViewController {
     
@@ -15,7 +16,18 @@ class MainViewController: UIViewController {
         label.textAlignment = .center
         label.text = "메인 화면"
         label.font = UIFont.boldSystemFont(ofSize: 70)
+        
         return label
+    }()
+    
+    // 애니메이션뷰
+    let animationView: LottieAnimationView = {
+        let animView = LottieAnimationView(name: "133076-welcome")
+        animView.frame = CGRect(x:0, y:0, width:400, height:400)   // 크기. x축과 y축 모두 0에서 시작
+        animView.contentMode = .scaleAspectFit   // 이미지를 확대할건지 축소할건지
+        
+        
+        return animView
     }()
 
     // 뷰가 생성되었을 때
@@ -23,13 +35,26 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.addSubview(titleLabel)
+        view.backgroundColor = .black
+        view.addSubview(animationView)
+        animationView.center = view.center
         
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        
+        // 애니메이션 실행
+        animationView.play{ (finish) in
+            // animationView.play가 실행되고 아래가 실행된다.
+            print("애니메이션이 끝났습니다.")
+            
+            // 클로저 안에 있을 때는 'self'를 명시해주어야 한다.
+            self.view.backgroundColor = .white
+            
+            // 애니메이션 끝내기
+            self.animationView.removeFromSuperview()
+            
+            self.view.addSubview(self.titleLabel)
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        }
     }
 }
 
